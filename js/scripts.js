d62a5a0f14c395e98b08efef25b7aa6734a4a765
davidabutler92 <davidabutler92@yahoo.com>
2020-03-23 14:17:55 -0700

    add jQuery librabry and scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+    alert('COVID-19 IS SCARY AND THIS IS A HEADER!')
+  });
+
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+});  
